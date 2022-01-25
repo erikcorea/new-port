c1963328e1502542d5641e68b8afec47503ee534
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-01-24 21:24:08 -0500

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "../styling/navbar.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">ERIK CORREA</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Projects</a>
+        <a href="/about">Skills</a>
+        <a href="/service">Contact</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
