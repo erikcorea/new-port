4a7a3b66054dd999a51f5670ee81715bc418d5d5
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-01-26 07:07:24 -0500

    about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
+      <About />
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styling/about.css";
+
+const About = () => {
+  
+  return (
+    <div className="about">
+        <div className="name">
+            <h1>Hi, Im Erik Correa.</h1>
+        </div>
+        <div className="title">
+            <h2>Software Engineer</h2>
+        </div>
+        <div className="about-paragraph">
+            <p>
+                Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur.
+                Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! 
+                Maiores minima consectetur 
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default About;
